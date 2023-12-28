5e06bc3d2ba58416ac6bdd596a4ad5531cb250b0
Christo <christo@devatstation.games>
2023-12-28 05:47:52 -0500

    removing more temp stuff, console on by default for now

--- Common/Console/Console.gd
+++ Common/Console/Console.gd
@@ -1,24 +1,22 @@
 extends TextEdit
 
 func setup_console():
-    await get_tree().create_timer(0.001).timeout
-    set_v_scroll(get_v_scroll_bar().max_value)
-    move_to_front()
-    hide()
-    set_smooth_scroll_enabled(true)
-    
+	await get_tree().create_timer(0.001).timeout
+	set_v_scroll(get_v_scroll_bar().max_value)
+	move_to_front()
+	#hide()
+	set_smooth_scroll_enabled(true)
+	
 func _ready():
-    setup_console()
-    
+	setup_console()
+	
 func _input(_event):
-    if Input.is_action_just_pressed("console"):
-        visible = !visible
-        if visible:
-            move_to_front()
-            set_v_scroll(get_v_scroll_bar().max_value)
-    return
-
+	if Input.is_action_just_pressed("console"):
+		visible = !visible
+		if visible:
+			move_to_front()
+			set_v_scroll(get_v_scroll_bar().max_value)
 
 func add(content):
-    text += content + "\n"
-    set_v_scroll(get_v_scroll_bar().max_value)
+	text += content + "\n"
+	set_v_scroll(get_v_scroll_bar().max_value)
